8d684ef800af034add776970456b2008f1889dcb
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-04-20 00:45:14 +0600

    yes you can

---
+++ src/Pages/Shared/QZone/QZone.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import qZone1 from '../../../assets/qZone1.png';
+import qZone2 from '../../../assets/qZone2.png';
+import qZone3 from '../../../assets/qZone3.png';
+
+
+
+const QZone = () => {
+    return (
+        <div className="">
+            <h4 className='mt-3'>QZone</h4>
+            <img src={qZone1} alt="" />
+            <img src={qZone2} alt="" />
+            <img src={qZone3} alt="" />
+        </div>
+        
+    );
+};
+
+export default QZone;
--- src/Pages/Shared/RightNav/RightNav.jsx
+++ src/Pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGoogle, FaGithub,FaFacebook,FaTwitter,FaInstagram } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png';
 
 const RightNav = () => {
     return (
         <div>
-              <h1>RightNav</h1>
+            <h4 className='mb-3'>Login With</h4>
+            <Button className='mb-2' variant="outline-secondary"><FaGoogle className='me-2'></FaGoogle>Login With Google</Button>
+            <Button variant="outline-success"><FaGithub className='me-2'></FaGithub>Login With Github</Button>
+            <div>
+                <h4 className='mt-3'>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook className='me-2'></FaFacebook> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter className='me-2'></FaTwitter>Twitter </ListGroup.Item>
+                    <ListGroup.Item><FaInstagram className='me-2'></FaInstagram>Instagram</ListGroup.Item>
+                    
+                </ListGroup>
+
+            </div>
+            <QZone></QZone>
+            <img src={bg} alt="" />
         </div>
     );
 };
