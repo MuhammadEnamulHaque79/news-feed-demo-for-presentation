66e15d997d86341c08dad8ec68f5c51a8b54b9d0
Muhammad Enamul Haque <muhammad.enamul.haque79@gmail.com>
2023-04-19 22:57:27 +0600

    updated

--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
 import moment from 'moment';
+import Button from 'react-bootstrap/Button';
+import { Container, Nav,Navbar } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p><small>Journalism Without Fear or Favour</small> </p>
                 {/* <p>Sunday, November 27, 2022</p> */}
-                <p>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
+                <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
-        </div>
+            <div className='d-flex'>
+                <Button variant="danger">Latest</Button>
+                <Marquee className='text-danger' speed={50} pauseOnHover={Boolean}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="mx-auto mt-3">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">About</Nav.Link>
+                            <Nav.Link href="#pricing">Career</Nav.Link>
+                        
+                        </Nav>
+                        <Nav className='align-items-center'>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                            <Button variant="dark">Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </Container>
     );
 };
 
